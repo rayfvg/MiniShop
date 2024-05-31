be5bc6959cb80e331f2fb940f7255d7aa462a287
rayfvg <rayfvg@yandex.ru>
2024-05-31 19:08:33 +0300

    update

--- MiniStore/Assets/Scripts/Goods.cs
+++ MiniStore/Assets/Scripts/Goods.cs
@@ -1,14 +1,16 @@
-using TMPro;
 using UnityEngine;
 
-[CreateAssetMenu (fileName = "new Goods", menuName = "Goods", order = 51)]
+[CreateAssetMenu(fileName = "new Goods", menuName = "Goods", order = 51)]
 public class Goods : ScriptableObject
 {
     [SerializeField] private string _lable;
     [SerializeField] private string _description;
     [SerializeField] private int _price;
-    [SerializeField] private Wallet _wallet;
-    public void ShowInfoByGoods() => Debug.Log($"Можем предложить {_lable}, {_description}, за {_price}."); 
 
+    public string Lable => _lable;
+    public string Description => _description;
+    public int Price => _price;
+
+    public void ShowInfoByGoods() => Debug.Log($"Можем предложить {_lable}, {_description}, за {_price}.");
     public void BuyGoods() => Debug.Log($"Вы купили {_lable} за {_price} денюжек");
 }
