6e84028e3bff8c1518250b9ec754bd1217b48535
xd-zede32x <homekor24@gmail.com>
2024-06-01 20:42:15 +0300

    ТЗ для Саши

---
+++ MiniStore/Assets/Scripts/Homework.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Homework : MonoBehaviour
+{
+    // 1 - Почистить папки проекта.
+    // 2 - Добавить namespace.
+    // 3 - Подумать как можно избавиться от дубляжа кода.
+}
